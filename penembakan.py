f2ef6e00bd3ecd2fd57f8dfa0320d26cf172c748
antoniusbayu76 <antoniusbayu96@gmail.com>
2023-12-27 16:31:04 +0700

    Update penembakan.py

--- penembakan.py
+++ penembakan.py
@@ -4,17 +4,31 @@ import numpy as np
 vid = cv2.VideoCapture(0)
 prevCir = None
 shoot = 15
+scalling = 1
 dist = lambda x1,y1,x2,y2: (x1-x2)**2*(y1-y2)**2
+
+def rescale(frame, scale=0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width, height)
+    return cv2.resize(frame, dimensions, interpolation=cv2.INTER_AREA)
+
+lower = np.array([20, 100, 100])
+upper = np.array([30, 255, 255])
 
 while True:
     ret, frame = vid.read()
     if not ret: break
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray,(21,21),0)
+    frame = rescale(frame,scalling)
 
-    circles = cv2.HoughCircles(blur, cv2.HOUGH_GRADIENT, 1.2, 200,
-                               param1=110, param2=50, minRadius=75, maxRadius=400)
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(frame,(21,21),0)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower, upper)
+
+    circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT, 1.2, 500,
+                               param1=100, param2=20, minRadius=75, maxRadius=300)
     
     cv2.line(frame, (0,frame.shape[0]//2), (frame.shape[1],frame.shape[0]//2), (0,0,0), thickness=2)
     cv2.line(frame, (frame.shape[1]//2,0), (frame.shape[1]//2,frame.shape[0]), (0,0,0), thickness=2)
@@ -46,6 +60,7 @@ while True:
         # print("x =", chosen[0])
         # print("y =", chosen[1])
     cv2.imshow("frame",frame)
+    cv2.imshow("mask",mask)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
