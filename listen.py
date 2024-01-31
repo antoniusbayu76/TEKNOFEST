796c6034b5107a4bbb5fb36730851839b6c9c55a
antoniusbayu76 <antoniusbayu96@gmail.com>
2024-01-31 13:40:26 +0700

    Update listen.py

--- listen.py
+++ listen.py
@@ -10,8 +10,11 @@ theConnection.mav.command_long_send(theConnection.target_system, theConnection.t
                                      mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,0,1,0,0,0,0,0,0)
 theConnection.mav.command_long_send(theConnection.target_system, theConnection.target_component,
                                      mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,0,0,0,0,0,0,0,10)
+theConnection.mav.send(mavutil.mavlink.MAVLink_set_position_target_local_ned_message(10,theConnection.target_system,
+                                                                                     theConnection.target_component,mavutil.mavlink.MAV_FRAME_LOCAL_NED,int(0b100111111000),10,0,-10,0,0,0,0,0,0,0,0))
 
 
 #     msg = theConnection.recv_match(blocking= True)
-msg = theConnection.recv_match(type='COMMAND_ACK',blocking=True)
+# msg = theConnection.recv_match(type='COMMAND_ACK',blocking=True)
+msg = theConnection.recv_match(type='LOCAL_PISITION_NED',blocking=True)
 print(msg)
